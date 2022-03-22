cf051574d29a7e225e4d726d291dc660c6b312d7
Talha Bhatti <80376242+talhabhathi@users.noreply.github.com>
2022-03-22 18:37:42 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -430,6 +430,9 @@ Route::prefix('admin')->middleware(['SessionCheck','auth'])->group(function () {
 
 
 
-Auth::routes();
+// Auth::routes();
+Auth::routes([
+    'register' => false
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
